f37cffb8fc7f3f75c0dde9a6a35a6fce69149417
LizaWebDev <e.kuntsevich03@gmail.com>
2025-09-07 10:19:31 +0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'christian-library-v1';
+const urlsToCache = [
+    '/',
+    '/static/js/bundle.js',
+    '/static/css/main.css',
+    '/manifest.json'
+];
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// eslint-disable-next-line no-restricted-globals
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => response || fetch(event.request))
+    );
+});
